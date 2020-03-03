00072fbc41f3a9555fd456c54212b1fdd08dbf1c
yang001 <18339052997@163.com>
2020-03-03 10:08:29 +0800

    003

--- Scripts/test_login.py
+++ Scripts/test_login.py
@@ -1,19 +1,22 @@
 import os
 import sys
-from time import sleep
-
 sys.path.append(os.getcwd())
+
+from time import sleep
 import pytest
 from Page.page_in import PageIn
 from Base.read_login_yaml import get_yaml
+from Base.read_login_txt import red_login_txt
 
-
-def get_data():
+def get_yaml_data():
     arrs = []
     for i in get_yaml().values():
         arrs.append((i.get("username"), i.get("pwd"), i.get("expect_resoult"), i.get("expect_toast") ))
         # arrs.append((i.get("username"), i.get("pwd"), i.get("expect_resoult")))
     return arrs
+
+def get_txt_data():
+   return red_login_txt("login_data.txt")
 
 # @allure.severity(allure.severity_level.CRITICAL)
 class TestLogin():
@@ -43,8 +46,12 @@ class TestLogin():
     # @allure.step("登录测试")
     # @allure.severity(allure.severity_level.CRITICAL)
     # @pytest.mark.parametrize("username, pwd, nickname", [("18339052997", "Zz0103", "lq_0223094916_yxe"), (" 1833", "22222", "此用户不存在")])
-    @pytest.mark.parametrize("username, pwd, expect_resoult, expect_toast", get_data())
-    # @pytest.mark.parametrize("username, pwd, expect_resoult", get_data())
+
+    # --- 以yaml读取
+    @pytest.mark.parametrize("username, pwd, expect_resoult, expect_toast", get_yaml_data())
+
+    # 以txt读取
+    # @pytest.mark.parametrize("username, pwd, expect_resoult, expect_toast", get_txt_data())
     def test_login(self, username, pwd, expect_resoult, expect_toast):
         if expect_resoult:
             try:
